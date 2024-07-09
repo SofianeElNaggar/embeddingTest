342cb9f59f72cd6acb2d621fc2afbb1c0b8ca244
SofianeElNaggar <sofiane.elnaggar@gmail.com>
2024-07-09 11:20:37 +0200

    utilisation des model encode decoder avec bart
    ça fonction bien (dans test.py)

--- bert_huggingface/test.py
+++ bert_huggingface/test.py
@@ -1,26 +1,37 @@
-from transformers import EncoderDecoderModel, AutoTokenizer
 import torch
-from get_embeding import *
+from transformers import AutoTokenizer, BartForConditionalGeneration
 
-# Charger le modèle et le tokenizer
-model_name = "patrickvonplaten/bert2bert_cnn_daily_mail"
-model = EncoderDecoderModel.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# Charger le tokenizer et le modèle BART pré-entraîné
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-large")
 
-# Le mot ou la phrase dont vous voulez obtenir l'embedding
-text = "example"
+# Exemple de texte d'entrée
+input_text = "Hello"
 
-# Tokeniser le texte
-inputs = tokenizer(text, return_tensors="pt")
+# Tokeniser le texte d'entrée
+inputs = tokenizer(input_text, return_tensors="pt")
 
-# Obtenir les embeddings du texte avec l'encodeur
+# Passer par l'encodeur pour obtenir les embeddings
 with torch.no_grad():
-    encoder_outputs = model.encoder(**inputs)
+    encoder_outputs = model.model.encoder(**inputs)
+    embedding = encoder_outputs.last_hidden_state
 
-# Récupérer le dernier état caché de l'encodeur
-encoder_hidden_states = encoder_outputs[0]
+print(embedding)
 
-print(encoder_outputs)
+# Initialiser l'entrée du décodeur avec le token de début de séquence
+decoder_start_token = model.config.decoder_start_token_id
+decoder_input_ids = torch.tensor([[decoder_start_token]])
 
+# Générer la séquence de sortie en utilisant les embeddings de l'encodeur
+output_sequences = model.generate(
+    input_ids=None,
+    encoder_outputs=encoder_outputs,
+    decoder_input_ids=decoder_input_ids,
+    max_length=50,
+    num_beams=5,
+    early_stopping=True
+)
 
-iterate_tokens(encoder_hidden_states, tokenizer, model)
+# Décoder les tokens de sortie en texte lisible
+result = tokenizer.decode(output_sequences[0], skip_special_tokens=True)
+print(result)
