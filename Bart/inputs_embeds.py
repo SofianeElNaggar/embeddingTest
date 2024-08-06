7fcba06e516d23edfce83227c2916f02ff6d60ec
SofianeElNaggar <sofiane.elnaggar@gmail.com>
2024-08-06 16:52:40 +0200

    nouveau test

--- Bart/inputs_embeds.py
+++ Bart/inputs_embeds.py
@@ -2,13 +2,14 @@ from transformers import BartTokenizer, BartForConditionalGeneration
 import torch
 from tools import *
 from  input_embedding_tools import *
+import time
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 tokenizer, model = load_tokenizer_and_model("facebook/bart-large")
 model.to(device)
 
-text = "hello"
+text = "Queen"
 
 inputs = tokenizer(text, return_tensors='pt').to(device)
 
@@ -18,11 +19,13 @@ input_embeddings = model.get_input_embeddings()
 inputs_embeds = input_embeddings(input_ids)
 
 
+start = time.time()
+sauvegarder_en_json(find_neighbor_around(model, tokenizer, device, input_embeddings, inputs_embeds, input_ids, min_lap=5),"./Bart/results/new test/neighbor/neighbor_Queen.json")
+end = time.time()
 
-sauvegarder_en_json(find_neighbor_around(model, tokenizer, device, input_embeddings, inputs_embeds, input_ids, min_lap=5),"./Bart/results/new test/neighbor/neighbor_hello.json")
+print(end - start)
 
-
-print("Embedding input before : \n" + str(inputs_embeds))
+"""print("Embedding input before : \n" + str(inputs_embeds))
 
 # Générer le texte avant la modification des embeddings
 text_before, batch_text_before = decode(model, tokenizer, input_ids, inputs_embeds)
@@ -38,3 +41,4 @@ set_new_embedding(model, device, input_embeddings, input_ids, modified_embeds)
 # Générer le texte après la modification des embeddings
 text_after, batch_text_after = decode(model, tokenizer, input_ids, inputs_embeds)
 print("Text after : \n" + text_after + "\n" + str(batch_text_after))
+"""
