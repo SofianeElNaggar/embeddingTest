6bda3c26dc0ec4c8a0b41ee6818c1166724f2676
SofianeElNaggar <sofiane.elnaggar@gmail.com>
2024-08-13 14:52:08 +0200

    Update main.py

--- Bart/rebuild_embedding/main.py
+++ Bart/rebuild_embedding/main.py
@@ -7,33 +7,30 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 tokenizer, model = tools.load_tokenizer_and_model("facebook/bart-large")
 model.to(device)
 
-print(get_closest_words(calculate_distances(" queen", "./Bart/inputs/bart_all_word_embedding.pkl"),10))
+def modif_embedding():
+    text = "hello"
+    inputs = tokenizer(text, return_tensors='pt').to(device)
+    input_ids, embedding, inputs_embeddings = get_embeddings(inputs, model)
+    print(inputs_embeddings)
 
 
-"""text = "Ļ"
+    print("Embedding input before : \n" + str(inputs_embeddings))
 
-inputs = tokenizer(text, return_tensors='pt').to(device)
+    # Générer le texte avant la modification des embeddings
+    text_before = decode(model, tokenizer, input_ids, inputs_embeddings)
+    print("Text before : \n" + text_before)
 
-# Obtenir les embeddings d'entrée
-input_ids, embedding, inputs_embeddings = get_embeddings(inputs, model)
+    # Modifier les embeddings d'entrée (ici, ajouter une petite valeur pour la démonstration)
+    modified_embeds = inputs_embeddings - inputs_embeddings
 
-print(inputs_embeddings)"""
+    print("\nEmbedding input after : \n" + str(modified_embeds))
 
+    set_new_embedding(model, device, embedding, input_ids, modified_embeds)
 
-"""print("Embedding input before : \n" + str(inputs_embeddings))
+    # Générer le texte après la modification des embeddings
+    text_after = decode(model, tokenizer, input_ids, inputs_embeddings)
+    print("Text after : \n" + text_after)
 
-# Générer le texte avant la modification des embeddings
-text_before = decode(model, tokenizer, input_ids, inputs_embeddings)
-print("Text before : \n" + text_before)
+#print(get_closest_words(calculate_distances(" queen", "./Bart/inputs/bart_all_word_embedding.pkl"),10))
 
-# Modifier les embeddings d'entrée (ici, ajouter une petite valeur pour la démonstration)
-modified_embeds = inputs_embeddings - inputs_embeddings
-
-print("\nEmbedding input after : \n" + str(modified_embeds))
-
-set_new_embedding(model, device, embedding, input_ids, modified_embeds)
-
-# Générer le texte après la modification des embeddings
-text_after = decode(model, tokenizer, input_ids, inputs_embeddings)
-print("Text after : \n" + text_after)
-"""
+#modif_embedding()
